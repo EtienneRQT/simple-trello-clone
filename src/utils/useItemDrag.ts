9002352eb4350f9b7caa518fbd238e0160dd1c60
Etienne <etienne.riquet@sfr.fr>
2021-11-10 23:21:51 +0100

    updated the column.tsx to implement the drag functionality

--- src/utils/useItemDrag.ts
+++ src/utils/useItemDrag.ts
@@ -3,6 +3,7 @@ import { useAppState } from "../state/AppStateContext";
 import { DragItem } from "../DragItem";
 import { useDrag } from "react-dnd";
 
+
 export const useItemDrag = (item: DragItem) => {
   const { dispatch } = useAppState();
   const [, drag] = useDrag({
