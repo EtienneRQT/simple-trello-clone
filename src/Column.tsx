a5e4afb0ec491fada055e9423f3f9173c1030885
Etienne <etienne.riquet@sfr.fr>
2021-11-12 20:57:06 +0100

    finished the core dragging functionality

--- src/Column.tsx
+++ src/Column.tsx
@@ -6,39 +6,38 @@ import { addTask, moveList } from "./state/actions";
 import { useRef } from "react";
 import { useItemDrag } from "./utils/useItemDrag";
 import { useDrop } from "react-dnd";
+import { isHidden } from "./utils/isHidden";
 
 type ColumnProps = {
   text: string;
   id: string;
 };
 
-
-
 export const Column = ({ text, id }: ColumnProps) => {
   const { draggedItem, getTasksByListId, dispatch } = useAppState();
   const tasks = getTasksByListId(id);
   const ref = useRef<HTMLDivElement>(null);
 
-const [, drop] = useDrop({
-  accept: "COLUMN",
-  hover() {
-    if (!draggedItem) {
-      return;
-    }
-    if (draggedItem.type === "COLUMN") {
-      if (draggedItem.id === id) {
+  const [, drop] = useDrop({
+    accept: "COLUMN",
+    hover() {
+      if (!draggedItem) {
         return;
       }
-      dispatch(moveList(draggedItem.id, id));
-    }
-  },
-});
+      if (draggedItem.type === "COLUMN") {
+        if (draggedItem.id === id) {
+          return;
+        }
+        dispatch(moveList(draggedItem.id, id));
+      }
+    },
+  });
 
   const { drag } = useItemDrag({ type: "COLUMN", id, text });
   drag(ref);
 
   return (
-    <ColumnContainer>
+    <ColumnContainer ref={ref} isHidden={isHidden(draggedItem, "COLUMN", id)}>
       <ColumnTitle>{text}</ColumnTitle>
       {tasks.map((task) => (
         <Card text={task.text} key={task.id} id={task.id} />
